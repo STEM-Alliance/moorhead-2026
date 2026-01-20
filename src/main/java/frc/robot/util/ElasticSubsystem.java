d65d6dd5d169204de45b3b1e5692d7fce94f99cc
NetLockJ <cokiehne@gmail.com>
2026-01-20 14:45:39 -0600

    Update .gitignore

--- src/main/java/frc/robot/util/ElasticSubsystem.java
+++ src/main/java/frc/robot/util/ElasticSubsystem.java
@@ -2,12 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.utils;
+package frc.robot.util;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.util.Elastic;
 import frc.robot.util.Elastic.Notification;
 import frc.robot.util.Elastic.Notification.NotificationLevel;
 
